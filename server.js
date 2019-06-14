ef2acf91169a22053474aed25d30f2a36c487334
oneil <oneilcampbell19@gmail.com>
2019-06-14 13:03:24 -0400

    made app more responsive

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 4567;
+const PORT = process.env.PORT || 4565;
 
 const path = require("path");
 // app.use("/", express.static("./build/"));
